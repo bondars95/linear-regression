928fd4630fdf7bdb6639071fc20254dd79df19c5
Serhii Bondar <sbondar@e1r1p11.unit.ua>
2018-07-29 14:46:59 +0300

    two seperate programs for training and estimating

--- src/main/java/com/unit/Predictor.java
+++ src/main/java/com/unit/Predictor.java
@@ -1,7 +1,38 @@
 package com.unit;
 
+import org.jfree.data.function.LineFunction2D;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Predictor {
-    public static void main(String[] args) {
-        System.out.println("Hi, I am predictor");
+    public static void main(String[] args) throws FileNotFoundException {
+        // If we have an input parameter, predict the price and draw the new point
+        if (args.length >= 1 && args[0] != null) {
+            // Estimate the linear function given the input data
+            double x = 0;
+            try {
+                x = Double.parseDouble(args[0]);
+            } catch (Exception e) {
+                System.out.println("Wrong mileauge received");
+                System.exit(1);
+            }
+            File file = new File("coefficients.txt");
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNext()) {
+                try {
+                    System.out.println(scanner.nextDouble() + scanner.nextDouble() * x);
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                    System.out.println("Parsing file error!");
+                    System.exit(1);
+                }
+            }
+        }
     }
 }
--- src/main/java/com/unit/Trainer.java
+++ src/main/java/com/unit/Trainer.java
@@ -2,11 +2,13 @@ package com.unit;
 
 import org.jfree.ui.RefineryUtilities;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 
-public class Main {
+public class Trainer {
     public static void main(String[] args) throws IOException {
         List<DatasetParser.DataSetEntry> res = Collections.emptyList();
         if (args.length != 1) {
@@ -23,8 +25,15 @@ public class Main {
                 res.stream().map(DatasetParser.DataSetEntry::getX).mapToDouble(Double::doubleValue).toArray(),
                 res.stream().map(DatasetParser.DataSetEntry::getY).mapToDouble(Double::doubleValue).toArray()
         );
+
         model.compute();
         double[] coefficients = model.getCoefficients();
+
+        // writing to coef file
+        BufferedWriter writer = new BufferedWriter(new FileWriter("coefficients.txt", false));
+        writer.append(coefficients[0] + " " + coefficients[1]);
+        writer.close();
+
         System.out.printf("Building regression line for: y = %.4f + %.4fx", coefficients[0], coefficients[1]);
         display(args, res, coefficients);
     }
@@ -40,21 +49,5 @@ public class Main {
         chart.setVisible(true);
         // Draw the regression line on the chart
         chart.drawRegressionLine(coefficients[0], coefficients[1]);
-        // todo move to separate program
-        // If we have an input parameter, predict the price and draw the new point
-//        if (args.length >= 1 && args[0] != null) {
-//            // Estimate the linear function given the input data
-//            double regressionParameters[] = Regression.getOLSRegression(
-//                    chart.inputData, 0);
-//            double x = Double.parseDouble(args[0]);
-//
-//            // Prepare a line function using the found parameters
-//            LineFunction2D linefunction2d = new LineFunction2D(
-//                    regressionParameters[0], regressionParameters[1]);
-//            // This is the estimated price
-//            double y = linefunction2d.getValue(x);
-//
-//            chart.drawInputPoint(x, y);
-//        }
     }
 }
